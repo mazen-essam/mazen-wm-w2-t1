29892dae7ae526fc4177dce5379cdadc2b4b9e1e
mazen-essam <mazenessam80@gmail.com>
2024-09-13 22:58:16 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"], // Adjust this path based on your project structure
+  theme: {
+    extend: {
+      colors:{
+        primary: '#1f1f1f',
+        secondary: '#545454',
+      }
+    },
+  },
+  plugins: [],
+};
